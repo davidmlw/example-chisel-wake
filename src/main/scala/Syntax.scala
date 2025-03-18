c073f7fa639fc9ac95abc89b596acce3f5b9d26b
Liwei Ma <liwei.ma@gmail.com>
2025-03-18 23:36:49 +0800

    bump version

--- src/main/scala/Syntax.scala
+++ src/main/scala/Syntax.scala
@@ -2,9 +2,8 @@ package syntax
 
 import chisel3._
 import chisel3.util._
-import chisel3.experimental.MultiIOModule
 
-class Null extends MultiIOModule {
+class Null extends Module {
   val a = IO(Input(UInt(16.W)))
   val b = IO(Input(UInt(16.W)))
   val c = IO(Output(UInt(16.W)))
